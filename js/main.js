59a32d09faae58dc07cee891ddf81bf3565eef8d
Alex Zarley <azarley@wisc.edu>
2016-04-24 19:21:39 -0500

    Data loaded in; pcp working

--- js/main.js
+++ js/main.js
@@ -3,145 +3,222 @@
 window.onload = setMap();
 //pseudo-global variables
 //array of variables used in for loops
-var attrArray = ["TIDE_M", "SLOPE_PCT", "ERR_M_YR", "GEOM", "WAVES_M", "CVIVALUE"];
-var expressed = attrArray[0]; //initial attribute
 
 //execute script when window is loaded
 
 function setMap() {
-  var margin = {top: 30, right: 10, bottom: 10, left: 10},
-    width = 960 - margin.left - margin.right,
-    height = 500 - margin.top - margin.bottom;
+    var pcpMargin = {top: 30, right: 10, bottom: 10, left: 10},
+      pcpWidth = window.innerWidth,
+      pcpWidth = pcpWidth - pcpMargin.left - pcpMargin.right,
+      pcpHeight = window.innerHeight * 0.25,
+      pcpHeight = pcpHeight - pcpMargin.top - pcpMargin.bottom;
 
-var x = d3.scale.ordinal().rangePoints([0, width], 1),
-    y = {},
-    dragging = {};
+    var x = d3.scale.ordinal().rangePoints([0, pcpWidth], 1),
+        y = {},
+        dragging = {};
 
-var line = d3.svg.line(),
-    axis = d3.svg.axis().orient("left"),
-    background,
-    foreground;
+    var line = d3.svg.line(),
+        axis = d3.svg.axis().ticks(4).orient("left"),
+        pcpBackground,
+        pcpForeground;
 
-var svg = d3.select("body").append("svg")
-    .attr("width", width + margin.left + margin.right)
-    .attr("height", height + margin.top + margin.bottom)
-  .append("g")
-    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+    var pcpSvg = d3.select("body").append("svg")
+        .attr("width", pcpWidth + pcpMargin.left + pcpMargin.right)
+        .attr("height", pcpHeight + pcpMargin.top + pcpMargin.bottom)
+      .append("g")
+        .attr("transform", "translate(" + pcpMargin.left + "," + pcpMargin.top + ")");
 
-d3.csv("data/CVIRaw.csv", function(error, csvFullData) {
-  var csvData = csvFullData.map(function(d) {
-      return {
-        CVIVALUE:  d.CVIVALUE,
-        ERR_M_YR: d.ERR_M_YR,
-        SLOPE_PCT: d.SLOPE_PCT,
-        SL_MM_YR_: d.SL_MM_YR_,
-        TIDE_M: d.TIDE_M,
-        GEOM: d.GEOM,
-        WAVES_M: d.WAVES_M
-      };
-  });
-  // Extract the list of dimensions and create a scale for each.
-  x.domain(dimensions = d3.keys(csvData[3]).filter(function(d) {
-    return d != "name" && (y[d] = d3.scale.linear()
-        .domain(d3.extent(csvData, function(p) { return +p[d]; }))
-        .range([height, 0]));
-  }));
+    d3.csv("data/Master.csv", function(error, attData) {
 
-  // Add grey background lines for context.
-  background = svg.append("g")
-      .attr("class", "background")
-    .selectAll("path")
-      .data(csvData)
-    .enter().append("path")
-      .attr("d", path);
 
-  // Add blue foreground lines for focus.
-  foreground = svg.append("g")
-      .attr("class", "foreground")
-    .selectAll("path")
-      .data(csvData)
-    .enter().append("path")
-      .attr("d", path);
+        //array to hold all property names
+        var allAttributes = [];
 
-  // Add a group element for each dimension.
-  var g = svg.selectAll(".dimension")
-      .data(dimensions)
-    .enter().append("g")
-      .attr("class", "dimension")
-      .attr("transform", function(d) { return "translate(" + x(d) + ")"; })
-      .call(d3.behavior.drag()
-        .origin(function(d) { return {x: x(d)}; })
-        .on("dragstart", function(d) {
-          dragging[d] = x(d);
-          background.attr("visibility", "hidden");
-        })
-        .on("drag", function(d) {
-          dragging[d] = Math.min(width, Math.max(0, d3.event.x));
-          foreground.attr("d", path);
-          dimensions.sort(function(a, b) { return position(a) - position(b); });
-          x.domain(dimensions);
-          g.attr("transform", function(d) { return "translate(" + position(d) + ")"; })
-        })
-        .on("dragend", function(d) {
-          delete dragging[d];
-          transition(d3.select(this)).attr("transform", "translate(" + x(d) + ")");
-          transition(foreground).attr("d", path);
-          background
-              .attr("d", path)
-            .transition()
-              .delay(500)
-              .duration(0)
-              .attr("visibility", null);
-        }));
+        //push property names from attData into allAttributes array
+        for (var keys in attData[0]){
+            allAttributes.push(keys);
+        };
 
-  // Add an axis and title.
-  g.append("g")
-      .attr("class", "axis")
-      .each(function(d) { d3.select(this).call(axis.scale(y[d])); })
-    .append("text")
-      .style("text-anchor", "middle")
-      .attr("y", -9)
-      .text(function(d) { return d; });
+        //create an array with ony properties with Raw values; for PCP display
+        var rawData = searchStringInArray("Raw", allAttributes);
 
-  // Add and store a brush for each axis.
-  g.append("g")
-      .attr("class", "brush")
-      .each(function(d) {
-        d3.select(this).call(y[d].brush = d3.svg.brush().y(y[d]).on("brushstart", brushstart).on("brush", brush));
-      })
-    .selectAll("rect")
-      .attr("x", -8)
-      .attr("width", 16);
+        //create an array with ony properties with Rank values; for calculation
+        var rankData = searchStringInArray("Rank", allAttributes);
+
+
+      //is there a way to do this without typing every one?
+      // //filters out properties from each line segment in csvFullData I don't want to display in PCP
+      // var filteredData = rankData.map(function(d) {
+      //
+      //     return {
+      //         Bars_Rank: d.Bars,
+      //         Bike: d.Bike,
+      //         Farmers_Markets: d.Farmers_Markets,
+      //         Fitness: d.Fitness,
+      //         Food: d.Food,
+      //         LGBTQ_Friendly: d.LGBTQ_Friendly,
+      //         MedianEarningsMonth: d.MedianEarningsMonth,
+      //         MedianRent: d.MedianRent,
+      //         Median_Earnings: d.Median_Earnings,
+      //         Museums: d.Museums,
+      //         Music: d.Music,
+      //         Parks: d.Parks,
+      //         Pet_Friendly: d.Pet_Friendly,
+      //         Price_of_Weed: d.Price_of_Weed,
+      //         Ratio_of_Males_to_Females: d.Ratio_of_Males_to_Females,
+      //         Safety: d.Safety,
+      //         Sports: d.Sports,
+      //         Transit: d.Transit,
+      //         Walk: d.Walk
+      //     }
+      // });
+
+      //what does this do overall??
+      // Extract the list of dimensions and create a scale for each.
+      //d3.keys(object) returns a new array containing all property values of object
+      //filter calls callback function once for each element in array
+      x.domain(dimensions = rankData.filter(function(d) {//d is each property name
+        // use this if you want to remove properties from an object "return d != "name" && ..."
+        return (y[d] = d3.scale.linear()
+            //function(p) is called before min is calculated - acts like array.map and runs function on every value in p
+            .domain(d3.extent(attData, function(p) { //p is each object in attData//how to deal with 0??
+                //returns every value for every property for every object with trailing 0's removed
+                return +p[d];
+            }))
+            .range([pcpHeight, 0]));
+      }));
+
+      // Add grey background lines for context.
+      pcpBackground = pcpSvg.append("g")
+          .attr("class", "background")
+        .selectAll("path")
+          .data(attData)
+        .enter().append("path")
+          .attr("d", path);
+
+      // Add blue foreground lines for focus.
+      pcpForeground = pcpSvg.append("g")
+          .attr("class", "foreground")
+        .selectAll("path")
+          .data(attData)
+        .enter().append("path")
+          .attr("d", path);
+
+      // Add a group element for each dimension.
+      var pcpG = pcpSvg.selectAll(".dimension")
+          .data(dimensions)
+        .enter().append("g")
+          .attr("class", "dimension")
+          .attr("transform", function(d) { return "translate(" + x(d) + ")"; })
+          .call(d3.behavior.drag()
+              .origin(function(d) { return {x: x(d)}; })
+              .on("dragstart", function(d) {
+                  dragging[d] = x(d);
+                  pcpBackground.attr("visibility", "hidden");
+              })
+              .on("drag", function(d) {
+                  dragging[d] = Math.min(width, Math.max(0, d3.event.x));
+                  pcpForeground.attr("d", path);
+                  dimensions.sort(function(a, b) { return position(a) - position(b); });
+                  x.domain(dimensions);
+                  pcpG.attr("transform", function(d) { return "translate(" + position(d) + ")"; })
+              })
+              .on("dragend", function(d) {
+                  delete dragging[d];
+                  transition(d3.select(this)).attr("transform", "translate(" + x(d) + ")");
+                  transition(pcpForeground).attr("d", path);
+              pcpBackground
+                  .attr("d", path)
+                  .transition()
+                  .delay(500)
+                  .duration(0)
+                  .attr("visibility", null);
+            })
+          );
+
+      // Add an axis and title.
+      var axes = pcpG.append("g")
+          .attr("class", "axes")
+          .each(function(d) {d3.select(this).call(axis.scale(y[d]))})
+        .append("text")
+          .style("text-anchor", "middle")
+          .attr("y", -9)
+          .text(function(d) { return d; });
+
+      // Add and store a brush for each axis.
+      pcpG.append("g")
+          .attr("class", "brush")
+          .each(function(d) {
+            d3.select(this).call(y[d].brush = d3.svg.brush().y(y[d]).on("brushstart", brushstart).on("brush", brush));
+          })
+        .selectAll("rect")
+          .attr("x", -8)
+          .attr("width", 16);
 });
 
 function position(d) {
-  var v = dragging[d];
-  return v == null ? x(d) : v;
+    var v = dragging[d];
+    return v == null ? x(d) : v;
 }
 
 function transition(g) {
-  return g.transition().duration(500);
+    return pcpG.transition().duration(500);
 }
 
 // Returns the path for a given data point.
 function path(d) {
-  return line(dimensions.map(function(p) { return [position(p), y[p](d[p])]; }));
+    return line(dimensions.map(function(p) { return [position(p), y[p](d[p])]; }));
 }
 
 function brushstart() {
-  d3.event.sourceEvent.stopPropagation();
+    d3.event.sourceEvent.stopPropagation();
 }
 
 // Handles a brush event, toggling the display of foreground lines.
 function brush() {
-  var actives = dimensions.filter(function(p) { return !y[p].brush.empty(); }),
-      extents = actives.map(function(p) { return y[p].brush.extent(); });
-  foreground.style("display", function(d) {
-    return actives.every(function(p, i) {
-      return extents[i][0] <= d[p] && d[p] <= extents[i][1];
-    }) ? null : "none";
-  });
+    var actives = dimensions.filter(function(p) { return !y[p].brush.empty(); }),
+        extents = actives.map(function(p) { return y[p].brush.extent(); });
+    pcpForeground.style("display", function(d) {
+        return actives.every(function(p, i) {
+            return extents[i][0] <= d[p] && d[p] <= extents[i][1];
+        }) ? null : "none";
+    });
 }
+
+//function to parse properties based on a string
+function searchStringInArray (str, strArray) {
+    var newArray = [];
+    for (var i=0; i<strArray.length; i++) {
+        if (strArray[i].match(str)) {
+            newArray.push(strArray[i]);
+        };
+    };
+    return newArray;
+};
+
+//replaces underscores in property names with spaces
+function removeUnderscores(array){
+    var newArray = [];
+    //remove underscores from strings in array
+    for (i=0; i<array.length; i++) {
+        var label = array[i]
+        label = label.split("_").join(" ") //converts underscores in csv to spaces for display purposes
+        newArray.push(label);
+    };
+    return newArray;
+};
+//replaces spaces in property names with underscores
+function addUnderscores(array){
+    var newArray = [];
+    //remove underscores from strings in array
+    for (i=0; i<array.length; i++) {
+        var label = array[i]
+        label = label.split(" ").join("_") //converts underscores in csv to spaces for display purposes
+        newArray.push(label);
+    };
+    return newArray;
+};
+
 
 };
 })();
